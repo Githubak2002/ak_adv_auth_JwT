9eb907f87a886a830363ea625e2e6eec20be671e
Githubak2002 <109411443+Githubak2002@users.noreply.github.com>
2024-09-19 02:38:29 +0530

    all set - auth done 98%

--- Backend/server.js
+++ Backend/server.js
@@ -2,16 +2,16 @@ import express from "express";
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import { connectDB } from "./connectDB.js";
 import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();            // to get access to .env
 
-// const __dirname = path.resolve();
 const app = express();
-
 const PORT = process.env.BACKEND_PORT || 8080;
+const __dirname = path.resolve();
 
 app.use(cors({ origin: ["http://localhost:5173"] , credentials: true }));
 app.use(express.json());    // parse incoming req with JSON payloads
@@ -20,10 +20,13 @@ app.use(cookieParser());    // parse incoming cookies
 
 
 // ==== static folder ====
-// app.use(express.static(path.join(__dirname,'/client/dist')));
-// app.get('*',(req,res) => {
-//   res.sendFile(path.join(__dirname,"client", "dist", "index.html"));
-// })
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
 
 
 app.get("/",(req,res)=>{
--- Frontend/src/Components/DeleteAccountPopup.jsx
+++ Frontend/src/Components/DeleteAccountPopup.jsx
@@ -1,44 +1,46 @@
-import React, { useState } from "react";
-import axios from "axios";
+import React from "react";
 import { useAuthStore } from "../../store/authStore";
 import { useNavigate } from "react-router-dom";
 
 const DeleteAccountPopup = () => {
+  const { deleteAccount, isLoading } = useAuthStore(); // Ensure the function is correctly named
+  const navigate = useNavigate(); 
 
-  const {deleteAcount, isLoading} = useAuthStore()
-  const navigete = useNavigate();
-
-
-  const sendRequestHandler = async (e) => {
+  const deleteAccountHandler = (e) => {
     e.preventDefault();
-    deleteAcount();
+    deleteAccount(); 
     useAuthStore.setState({ deleteAccPopup: false });
-    navigete('/');
+    navigate('/'); 
   };
 
+  const cancelHandler = (e) => {
+    e.preventDefault();
+    useAuthStore.setState({ deleteAccPopup: false });
+  };
 
   return (
-    <section className="">
-      <main className="fixed inset-0 flexCenter bg-black bg-opacity-50 backdrop-blur-sm px-4">
-        <form
-          onSubmit={sendRequestHandler}
-          className="w-full max-w-sm bg-white p-6 border border-gray-300 rounded-lg shadow-lg"
-        >
-          <h2 className="sm:text-2xl text-xl mb-4 leading-9 py-4">
-            Are you sure you want to delete your account?
-          </h2>
+    <section className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 backdrop-blur-sm px-4">
+      <main className="w-full max-w-sm bg-white p-6 border border-gray-300 rounded-lg shadow-lg">
+        <h2 className="sm:text-2xl text-xl mb-4 leading-9 py-4">
+          Are you sure you want to delete your account?
+        </h2>
 
-          <div className="flexCenter">
-            <button
-              type="submit"
-              className={`text-white py-2 px-4 mb-3 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 ${isLoading ? "bg-blue-300" : "bg-blue-500"} ` }
-              disabled={isLoading} 
-            >
-              { isLoading ? "Deleting..." : "Delete"}
-            </button>
-          </div>
+        <div className="flexCenter gap-x-5">
+          <button
+            onClick={deleteAccountHandler}
+            className={`text-white py-2 px-4 mb-3 rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 ${isLoading ? "bg-blue-300" : "bg-blue-500"}`}
+            disabled={isLoading} 
+          >
+            {isLoading ? "Deleting..." : "Delete"}
+          </button>
 
-        </form>
+          <button
+            onClick={cancelHandler}
+            className="py-2 px-4 mb-3 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 border border-slate-500"
+          >
+            Cancel
+          </button>
+        </div>
       </main>
     </section>
   );
