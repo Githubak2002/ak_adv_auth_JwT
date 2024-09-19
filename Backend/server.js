541d715577c2ecb1367adccbcfa22c253af3e466
Githubak2002 <109411443+Githubak2002@users.noreply.github.com>
2024-09-19 12:13:53 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -21,10 +21,10 @@ app.use(cookieParser());    // parse incoming cookies
 
 // ==== static folder ====
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.use(express.static(path.join(__dirname, "/Frontend/dist")));
 
 	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+		res.sendFile(path.resolve(__dirname, "Frontend", "dist", "index.html"));
 	});
 }
 
